eb2b593ce4bcd9788fa574152adb64528601e91e
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2024-11-02 14:06:08 +0200

    Change

--- LabTask1/android-pzpi-23-8-khudiienko-mykyta-lab-task1/app/src/main/java/nure/khudiienkomykyta/labtask1/MainActivity.java
+++ LabTask1/android-pzpi-23-8-khudiienko-mykyta-lab-task1/app/src/main/java/nure/khudiienkomykyta/labtask1/MainActivity.java
@@ -23,36 +23,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Log.d(TAG, "onCreate");
+        Log.d(TAG, "onCreated");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart");
+        Log.d(TAG, "onStarted");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
+        Log.d(TAG, "onResumed");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause");
+        Log.d(TAG, "onPaused");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop");
+        Log.d(TAG, "onStoped");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "onDestroyed");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestarted");
     }
 }
