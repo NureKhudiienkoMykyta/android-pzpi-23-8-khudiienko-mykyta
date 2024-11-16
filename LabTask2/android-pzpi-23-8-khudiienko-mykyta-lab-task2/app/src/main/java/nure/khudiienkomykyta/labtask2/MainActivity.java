efe40859dd758db192c5c1a5041ff007c440d139
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2024-11-16 21:58:50 +0200

    LabTask2

--- LabTask2/android-pzpi-23-8-khudiienko-mykyta-lab-task2/app/src/main/java/nure/khudiienkomykyta/labtask2/MainActivity.java
+++ LabTask2/android-pzpi-23-8-khudiienko-mykyta-lab-task2/app/src/main/java/nure/khudiienkomykyta/labtask2/MainActivity.java
@@ -1,8 +1,12 @@
 package nure.khudiienkomykyta.labtask2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +14,60 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View colorPanel;
+    private SeekBar redSeekBar, blueSeekBar, greenSeekBar;
+    private int red, green, blue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        colorPanel = findViewById(R.id.view);
+        redSeekBar = findViewById(R.id.seekBarRed);
+        greenSeekBar = findViewById(R.id.seekBarGreen);
+        blueSeekBar = findViewById(R.id.seekBarBlue);
+
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (seekBar == redSeekBar) red = progress;
+                if (seekBar == greenSeekBar) green = progress;
+                if (seekBar == blueSeekBar) blue = progress;
+                updateColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        };
+
+        redSeekBar.setOnSeekBarChangeListener(listener);
+        greenSeekBar.setOnSeekBarChangeListener(listener);
+        blueSeekBar.setOnSeekBarChangeListener(listener);
+
+    }
+
+    private void updateColor() {
+        colorPanel.setBackgroundColor(Color.rgb(red, green, blue));
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("red", red);
+        outState.putInt("green", green);
+        outState.putInt("blue", blue);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        red = savedInstanceState.getInt("red", 0);
+        green = savedInstanceState.getInt("green", 0);
+        blue = savedInstanceState.getInt("blue", 0);
+        updateColor();
     }
 }
